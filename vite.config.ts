e742feaddf7350e51b5c8d31538a83b6fc9011d3
AnnV <annvasilyeva89@gmail.com>
2026-05-22 08:40:06 +0500

    setup github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/game_xstate_react_ts/',
   server: {
     port: 5173,
     open: true,
